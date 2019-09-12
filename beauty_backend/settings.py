c8a155b9450b29aae0df5d413275c032738623a0
Kayo Riccelo <kayo@layoutsistemas.com.br>
2019-09-12 13:39:20 -0300

    Organizando INSTALLED_APPs;

--- beauty_backend/settings.py
+++ beauty_backend/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_APPS = [
+    ''
+]
+
+BEAUTY_APP = [
+    'core.user',
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + BEAUTY_APP
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -103,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
