e442f4e83b3a24c088d05ba580c0803283f4ab08
Kayo Riccelo <kayo.riccelo@hotmail.com>
2019-09-13 15:59:15 -0300

    Adicionado fk provider em product;

--- apps/registrations/product/models.py
+++ apps/registrations/product/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     code = models.CharField(max_length=10)
     description = models.CharField(max_length=140)
+    provider = models.ManyToManyField('provider.Provider', verbose_name='provider', related_name='product')
 
     class Meta:
         verbose_name = 'product'
